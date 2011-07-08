0b6b7477b8bfb6ee98e635918e824ae98684d829
David Erickson <derickso@stanford.edu>
2011-07-08 16:03:41 -0700

    Cleaned up admin config page
    Visual feedback added when the config is saved,
    also fixed the left side menu being properly
    highlighted when on this page.

--- src/main/resources/config.js
+++ src/main/resources/config.js
@@ -16,7 +16,11 @@ AJS.toInit(function() {
             type: "PUT",
             contentType: "application/json",
             data: '{ "publicKey": "' + AJS.$("#publicKey").attr("value") + '", "privateKey": "' +  AJS.$("#privateKey").attr("value") + '" }',
-            processData: false
+            processData: false,
+            success: function(msg) {
+                AJS.$('#adminSubmitButton').attr('disabled', 'disabled');
+                AJS.$('#adminSubmitButton').attr('value', savedMessage);
+            }
         });
     }     
     populateForm();
@@ -24,5 +28,10 @@ AJS.toInit(function() {
     AJS.$("#admin").submit(function(e) {
         e.preventDefault();
         updateConfig();
+    });
+
+    AJS.$("#admin input[type='text']").keypress(function() {
+        AJS.$('#adminSubmitButton').removeAttr('disabled');
+        AJS.$('#adminSubmitButton').attr('value', saveMessage);
     });
 });
